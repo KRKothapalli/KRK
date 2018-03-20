1134090a44c8edcd1deca7a827eef461d1efb007
Krishna Kothapalli <kothapak@pryor.local>
2018-03-20 16:34:20 -0500

    initial commit

---
+++ RainbowPoop/EmojiPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RainbowPoop
+{
+    public class EmojiPlugin
+    {
+        public static string InsertEmoji(string subject)
+        {
+            var regEx = new Regex(@":\w*\:");
+            var emojiPlaceHolders = regEx.Matches(subject);
+            var emojiTag = string.Empty;
+
+            for (int i = 0; i < emojiPlaceHolders.Count; i++)
+            {
+                emojiTag = emojiPlaceHolders[i].Value.ToString().Replace(":", string.Empty).ToLower();
+
+                var emoji = GetEmojiByTagName(emojiTag);
+
+                if (emoji == null)
+                    subject = subject.Replace(emojiPlaceHolders[i].Value, "");
+                else
+                    subject = subject.Replace(emojiPlaceHolders[i].Value, emoji.ToString());
+            }
+            return subject;
+        }
+
+        public static object GetEmojiByTagName(string emojiTagName)
+        {
+            var emoji = typeof(Centvrio.Emoji.FacePositive).Assembly.GetTypes()
+                .SelectMany(x => x.GetFields())
+                .FirstOrDefault(x => x.Name.ToLower() == emojiTagName);
+
+            if (emoji != null)
+            {
+                return emoji.GetValue(null);
+            }
+            return null;
+        }
+    }
+}
