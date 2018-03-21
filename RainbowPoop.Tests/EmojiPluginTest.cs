d4810895a006d5d34437bad84df80bfd52e69976
Krishna Kothapalli <kothapak@pryor.local>
2018-03-21 13:47:44 -0500

    Another test.

--- RainbowPoop.Tests/EmojiPluginTest.cs
+++ RainbowPoop.Tests/EmojiPluginTest.cs
@@ -18,5 +18,12 @@ namespace RainbowPoop.Tests
             var result = EmojiPlugin.InsertEmoji("hello world :Smiling:");
             Assert.AreEqual("hello world ☺", result);
         }
+
+        [TestMethod]
+        public void It_inserts_Smiling_when_upper_case()
+        {
+            var result = EmojiPlugin.InsertEmoji("hello world :SMILING:");
+            Assert.AreEqual("hello world ☺", result);
+        }
     }
 }
