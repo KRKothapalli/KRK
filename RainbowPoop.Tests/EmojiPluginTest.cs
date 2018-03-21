7899504105c0e41a279c033532fcaba5d20236c2
Krishna Kothapalli <kothapak@pryor.local>
2018-03-21 13:46:42 -0500

    First passing test.

---
+++ RainbowPoop.Tests/EmojiPluginTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RainbowPoop.Tests
+{
+    [TestClass]
+    public class EmojiPluginTest
+    {
+
+            [TestMethod]
+            public void It_inserts_Smiling()
+            {
+                var result = EmojiPlugin.InsertEmoji("hello world :Smiling:");
+                Assert.AreEqual("hello world ☺", result);
+            }
+    }
+}
